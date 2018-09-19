3f1555c7b99a05fa90cd609dd439496752dcbcd1
chenyuwen <chenyuwen@meizu.com>
2018-09-19 15:15:02 +0800

    [Even] Add friends table.

--- mysql_db.sql
+++ mysql_db.sql
@@ -1,15 +1,32 @@
 CREATE DATABASE IF NOT EXISTS geekchat;
 USE geekchat;
 CREATE TABLE IF NOT EXISTS users (
-    `usrid` INT UNSIGNED KEY AUTO_INCREMENT,
+    `uid` INT UNSIGNED KEY AUTO_INCREMENT,
     `username` VARCHAR(20) NOT NULL,
     `password` VARCHAR(40) NOT NULL,
     `age` INT,
     `submission_date` DATE,
-    `sex` ENUM("man", "woman", "null")
+    `sex` ENUM("man", "woman", "null"),
+    UNIQUE KEY `catename` (`username`)
 ) DEFAULT CHARSET=utf8;
-INSERT INTO geekchat.users(usrid, username, password) VALUES ("1", "root", "root");
-INSERT INTO geekchat.users(usrid, username, password) VALUES ("2", "zhangsan", "lisi");
-INSERT INTO geekchat.users(usrid, username, password) VALUES ("3", "lisi", "lisi");
+ALTER TABLE geekchat.users AUTO_INCREMENT = 1;
+INSERT INTO geekchat.users(username, password) VALUES ("root", "root");
+INSERT INTO geekchat.users(username, password) VALUES ("zhangsan", "zhangsan");
+INSERT INTO geekchat.users(username, password) VALUES ("lisi", "lisi");
+INSERT INTO geekchat.users(username, password) VALUES ("liming", "liming");
+
+CREATE TABLE IF NOT EXISTS friends (
+    `fid` INT UNSIGNED KEY AUTO_INCREMENT,
+    `username` VARCHAR(20) NOT NULL,
+    `friend` VARCHAR(20) NOT NULL,
+    UNIQUE KEY `catename` (`username`, `friend`)
+) DEFAULT CHARSET=utf8;
+ALTER TABLE geekchat.friends AUTO_INCREMENT = 1;
+INSERT INTO geekchat.friends(username, friend) VALUES ("root", "zhangsan");
+INSERT INTO geekchat.friends(username, friend) VALUES ("root", "lisi");
+INSERT INTO geekchat.friends(username, friend) VALUES ("root", "liming");
+INSERT INTO geekchat.friends(username, friend) VALUES ("lisi", "zhangsan");
+INSERT INTO geekchat.friends(username, friend) VALUES ("lisi", "liming");
+
 CREATE user geekchat@localhost IDENTIFIED BY '1234';
 GRANT ALL ON geekchat.* TO 'geekchat'@'localhost';
