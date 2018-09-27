af778904243c15a440145cd8df0652767ac48e71
chenyuwen <chenyuwen@meizu.com>
2018-09-27 18:02:49 +0800

    [Even] change the client alive time to 30min.

--- client.c
+++ client.c
@@ -17,8 +17,8 @@
 #include "mlog.h"
 
 #define SERVER_DEFAULT_PORT 1200
-//#define SERVER_DEFAULT_ADDR "45.32.82.181"
-#define SERVER_DEFAULT_ADDR "127.0.0.1"
+#define SERVER_DEFAULT_ADDR "45.32.82.181"
+//#define SERVER_DEFAULT_ADDR "127.0.0.1"
 #define CLIENT_DUMP 0
 #define STDIN 0
 
